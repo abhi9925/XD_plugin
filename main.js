509fa23a36d8bcc436cf45e503e0162433c09356
abhi9925 <30955441+abhi9925@users.noreply.github.com>
2020-05-05 15:33:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const commands = require("commands");
+
+function updateLayout2(selection) {
+    // console.log(selection.items);
+    
+    
+    if(selection.items.length !== 0){
+
+        selection.items.forEach((element) => {
+            element.width = 300;
+            element.height = 100;
+            
+        });
+
+    }
+}
+
+module.exports = {
+  commands: {
+    updateLayout2
+  }
+};
+

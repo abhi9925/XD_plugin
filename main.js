ae9314ae43e0536068d40b659adc7919a76a4c0b
abhi9925 <30955441+abhi9925@users.noreply.github.com>
2020-05-07 12:38:18 +0530

    User enters height, width, colour

--- main.js
+++ main.js
@@ -1,22 +1,97 @@
-const commands = require("commands");
+const { selection } = require("scenegraph")
+let panel;
 
-function updateLayout2(selection) {
-    // console.log(selection.items);
-        
-    if(selection.items.length !== 0){
+function create() {
+    const HTML =
+        `<style>
+            .break {
+                flex-wrap: wrap;
+            }
+            label.row > span {
+                color: #8E8E8E;
+                width: 20px;
+                text-align: right;
+                font-size: 9px;
+            }
+            label.row input {
+                flex: 1 1 auto;
+            }
+            .show {
+                display: block;
+            }
+            .hide {
+                display: none;
+            }
+        </style>
+        <form method="dialog" id="main">
+        <h1>Enter the height, width and colour you want to fill</h1>
+            <div class="row break">
+                <label class="row">
+                    <input type="number"  id="txtV"  placeholder="Height" />
+                </label>
+                <label class="row">
+                    <input type="number"  id="txtH"  placeholder="Width" />
+                </label>
+                <label class="row">
+                    <input type="color" id="clr" value="#FFFFFF" placeholder="Hex code" />
+                </label>
+            </div>
+    
+            <button id="ok" type="submit" uxp-variant="cta" align="left">Apply</button>
+        </form>
+        <p id="warning">This plugin requires you to select a rectangle in the document. Please select a rectangle.</p>
+        `
+    function updateRectangleSize() {
+        const { editDocument } = require("application");
+        const Color = require("scenegraph").Color;
+        const height = Number(document.querySelector("#txtV").value);
+        const width = Number(document.querySelector("#txtH").value);
+        const colr = new Color(document.getElementById("clr").value);
 
-        selection.items.forEach((element) => {
-            element.width = 300;
-            element.height = 100;
+
+        editDocument({ editLabel: "Update rectangle size" }, function (selection) {
+            const selectedRectangle = selection.items[0];
+            if(width !== 0){
+                selectedRectangle.width = width
+            }
+            if(height !== 0){
+                selectedRectangle.height = height
+            }
             
-        });
+            selectedRectangle.fill = colr;
+        })
+    }
 
+    panel = document.createElement("div");
+    panel.innerHTML = HTML;
+    panel.querySelector("form").addEventListener("submit", updateRectangleSize);
+
+    return panel;
+}
+
+function show(event) {
+    if (!panel) event.node.appendChild(create());
+}
+
+function update() {
+    const { Rectangle } = require("scenegraph");
+    let form = document.querySelector("form");
+    let warning = document.querySelector("#warning");
+    if (!selection || !(selection.items[0] instanceof Rectangle)) {
+        form.className = "hide";
+        warning.className = "show";
+    } else {
+        form.className = "show";
+        warning.className = "hide";
     }
 }
 
-module.exports = {
-  commands: {
-    updateLayout2
-  }
-};
 
+module.exports = {
+    panels: {
+        updateLayout: {
+            show,
+            update
+        }
+    }
+};
